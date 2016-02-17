9aad15d34f30e57e59e80375a1722013534e78f3
VratislavHais <HaisVratislav@seznam.cz>
2016-02-17 10:15:06 +0100

    Skeleton

---
+++ app/components/about/about.ts
@@ -0,0 +1,8 @@
+import {Component, CORE_DIRECTIVES} from 'angular2/angular2';
+
+@Component({
+  selector: 'about',
+  templateUrl: './components/about/about.html',
+  directives: [CORE_DIRECTIVES]
+})
+export class AboutCmp {}
---
+++ app/components/app/app.ts
@@ -0,0 +1,22 @@
+import {Component, ViewEncapsulation} from 'angular2/angular2';
+import {
+  RouteConfig,
+  ROUTER_DIRECTIVES
+} from 'angular2/router';
+// import {HTTP_PROVIDERS} from 'http/http';
+
+import {HomeCmp} from '../home/home';
+import {AboutCmp} from '../about/about';
+
+@Component({
+  selector: 'app',
+  templateUrl: './components/app/app.html',
+  styleUrls: ['./components/app/app.css'],
+  encapsulation: ViewEncapsulation.None,
+  directives: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+  { path: '/', component: HomeCmp, as: 'Home' },
+  { path: '/about', component: AboutCmp, as: 'About' }
+])
+export class AppCmp {}
