3e63c8fa941fa63f8ab7f179e584cc0a8d8cdaaa
mciz <xcizma05@stud.fit.vutbr.cz>
2016-05-05 11:17:02 +0200

    First working Druid query

--- infispector_app/server/api/druidApi.js
+++ infispector_app/server/api/druidApi.js
@@ -1,6 +1,33 @@
 var app = require('../../app.js');
 
-exports.queryDruid = function (req, res) {    
-    console.log("Called queryDruid function from druidApi.js.");
-    res.send({error: 0, jsonObjects: "queryDruid function from druidApi.js called, check console log"}, 201);
+exports.queryDruid = function (request, response) {
+
+   console.log('queryDruid function from druidApi.js was called. '
+           + request.body.payload + " " + request.body.myQuery);
+
+   var params = {host: "127.0.0.1:8084", debug: "true"};
+   var druidRequester = require('facetjs-druid-requester').druidRequesterFactory(params);
+
+   druidRequester({
+       query: {
+           "queryType": "topN",
+           "dataSource": "InfiSpectorTopic",
+           "granularity": "all",
+           "dimension": "dest",
+           "metric": "length",
+           "threshold": 5,
+           "aggregations": [
+               {"type": "count", "fieldName": "length", "name": "length"}
+           ],
+           "intervals": ["2009-10-01T00:00/2020-01-01T00"]
+       }
+   })
+           .then(function (result) {
+               console.log('***Result: ', result);
+               
+               response.send({error: 0, jsonResponseAsString: JSON.stringify(result[0])}, 201);
+               //response.json("{result: " + result[0].result[1] + " }");
+           })
+
+           .done();
 };
