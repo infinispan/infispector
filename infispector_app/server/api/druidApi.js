bc5ba7213cd2610f0e9a9d47d445e97f3605d4aa
mciz <xcizma05@stud.fit.vutbr.cz>
2016-06-08 10:29:10 +0200

    DruidApi added getNodes, getMessagesCount

--- infispector_app/server/api/druidApi.js
+++ infispector_app/server/api/druidApi.js
@@ -1,8 +1,9 @@
 var app = require('../../app.js');
 
+//specimen 
 exports.queryDruid = function (request, response) {
 
-   console.log('queryDruid function from druidApi.js was called. '
+ console.log('queryDruid function from druidApi.js was called. '
            + request.body.payload + " " + request.body.myQuery);
 
    var params = {host: "127.0.0.1:8084", debug: "true"};
@@ -28,16 +29,15 @@ exports.queryDruid = function (request, response) {
                response.send({error: 0, jsonResponseAsString: JSON.stringify(result[0])}, 201);
                //response.json("{result: " + result[0].result[1] + " }");
            })
-
            .done();
 };
 
 
-// function which returns field of nodes 
+// function that returns field of nodes 
+//// add null to nodes in .getNodes method
 exports.getNodes = function (request, response) {
 
-   console.log('queryDruid function from druidApi.js was called. '
-           + request.body.payload + " " + request.body.myQuery);
+   console.log('getNodes function from druidApi.js was called.');
 
    var params = {host: "127.0.0.1:8084", debug: "true"};
    var druidRequester = require('facetjs-druid-requester').druidRequesterFactory(params);
@@ -49,7 +49,72 @@ exports.getNodes = function (request, response) {
            "granularity": "all",
            "dimension": "dest",
            "metric": "length",
-           "threshold": 5,
+           "threshold": 100000,
+           "aggregations": [
+               {"type": "count", "fieldName": "length", "name": "length"}
+           ],
+           "intervals": ["2009-10-01T00:00/2020-01-01T00"]
+       }
+   })
+           .then(function (result) {
+       
+               var test = JSON.stringify(result[0]);
+               var reg = /(?:\"dest\"):\"[a-zA-Z]+-\d+/g; 
+               var nodeField = test.match(reg);
+
+               for (var i = 0; i < nodeField.length; i++){
+                   nodeField[i] = nodeField[i].replace('"dest":"',"");
+
+               }
+               console.log("\n\nResult: "+ nodeField);
+               
+               response.send({error: 0, jsonResponseAsString: JSON.stringify(nodeField)}, 201);
+           })
+           .done();
+};
+
+
+/*
+@brief function which returns final count of messages from src node to dest node
+@param srcNode
+@param destNode
+@return JSON 
+*/
+
+exports.getMessagesCount = function (request, response) {
+
+  console.log('getMessagesCount function from druidApi.js was called. '
+           + request.body.srcNode + " " + request.body.destNode);
+    
+   var srcNode = request.body.srcNode;
+   var destNode = request.body.destNode;
+     
+   var params = {host: "127.0.0.1:8084", debug: "true"};
+   var druidRequester = require('facetjs-druid-requester').druidRequesterFactory(params);
+
+   druidRequester({
+       query: {
+           "queryType": "topN",
+           "dataSource": "InfiSpectorTopic",
+           "granularity": "all",
+           "dimension": "length",
+           "metric": "length",
+           "threshold": 10000,
+           "filter": {
+		   "type": "and",
+		   "fields": [
+		      {
+		        "type": "selector",
+		        "dimension": "src",
+		        "value": srcNode
+		      },
+		      {
+		        "type": "selector",
+		        "dimension": "dest",
+		        "value": destNode
+		      }
+		    ]
+		  },
            "aggregations": [
                {"type": "count", "fieldName": "length", "name": "length"}
            ],
@@ -58,20 +123,81 @@ exports.getNodes = function (request, response) {
    })
             
            .then(function (result) {
-       
-               var test = JSON.stringify(result[0]);
-               var reg = /(?:\"dest\"):\"[a-zA-Z]+-\d+/g; 
-               var nodes = test.match(reg);
-
-               for (var i = 0; i < nodes.length; i++){
-                   nodes[i] = nodes[i].replace('"dest":"',"");
-
-               }
-               console.log("\n\nResult: "+ nodes);
+                
+               var test = JSON.stringify(result[0]); 
+               var reg = /(?:"length":)[0-9]+/g; 
+               var messagesCount = test.match(reg);      
+               messagesCount = messagesCount[0].replace('"length":',"");
                
-               response.send({error: 0, jsonResponseAsString: JSON.stringify(nodes)}, 201);
+               console.log(messagesCount);
                
+               response.send({error: 0, jsonResponseAsString: JSON.stringify(messagesCount)}, 201);
            })
+           .done();
+};
 
+/*
+@brief function which returns final count of messages from src node to dest node and from given time interval
+@param srcNode
+@param destNode
+@param fromTime
+@param toTime
+@return string
+*/
+
+exports.getMessagesCountInInterval = function (request, response) {
+
+ console.log('getMessagesCountInInterval function from druidApi.js was called. '
+           + request.body.srcNode + " " + request.body.destNode
+           + " " + request.body.fromTime + " " + request.body.toTime);
+    
+   var srcNode = request.body.srcNode;
+   var destNode = request.body.destNode;
+   var fromTime = request.body.fromTime;
+   var toTime = request.body.toTime;
+    
+   var params = {host: "127.0.0.1:8084", debug: "true"};
+   var druidRequester = require('facetjs-druid-requester').druidRequesterFactory(params);
+
+   druidRequester({
+       query: {
+           "queryType": "topN",
+           "dataSource": "InfiSpectorTopic",
+           "granularity": "all",
+           "dimension": "length",
+           "metric": "length",
+           "threshold": 10000,
+           "filter": {
+		   "type": "and",
+		   "fields": [
+		      {
+		        "type": "selector",
+		        "dimension": "src",
+		        "value": srcNode
+		      },
+		      {
+		        "type": "selector",
+		        "dimension": "dest",
+		        "value": destNode
+		      }
+		    ]
+		  },
+           "aggregations": [
+               {"type": "count", "fieldName": "length", "name": "length"}
+           ],
+           "intervals": [fromTime + "/" + toTime]
+       }
+   })
+           .then(function (result) {
+                
+               var test = JSON.stringify(result[0]); 
+               var reg = /(?:"length":)[0-9]+/g; 
+               var messagesCount = test.match(reg);      
+               messagesCount = messagesCount[0].replace('"length":',"");
+               
+               console.log(messagesCount);
+               
+               response.send({error: 0, jsonResponseAsString: JSON.stringify(messagesCount)}, 201);
+           })
            .done();
 };
