8f6c126d4289d6aa138eba97ce1de91c3e57f9da
mciz <marek.ciz@email.cz>
2017-04-12 14:55:15 +0200

    Openshift project structure

---
+++ app/js/directives.js
@@ -0,0 +1,10 @@
+/*! InfiSpector - v0.0.1 - 2017-04-12
+ * https://github.com/infinispan/infispector/
+ * Copyright (c) 2017 ;
+ * Licensed 
+ */
+app.directive('stuff', function () {
+    return {        
+        template: 'Some stuff from my custom directive.'
+    };
+});
